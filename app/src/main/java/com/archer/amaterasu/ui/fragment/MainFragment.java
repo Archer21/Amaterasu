2aef66426444d704271d53a76f9c9b297cf0765d
Alan Aliaga <alanaliaga@MacBook-Pro-de-Alan.local>
2016-04-18 00:14:13 -0400

    Agregados tabs en fragmento, proceder a refactorizar usando una interface

--- app/src/main/java/com/archer/amaterasu/ui/fragment/MainFragment.java
+++ app/src/main/java/com/archer/amaterasu/ui/fragment/MainFragment.java
@@ -1,30 +1,55 @@
 package com.archer.amaterasu.ui.fragment;
 
-
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.archer.amaterasu.R;
+import com.archer.amaterasu.common.BaseFragment;
+import com.archer.amaterasu.utils.PagerAdapter;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends BaseFragment {
 
-
-    public MainFragment() {
-        // Required empty public constructor
-    }
-
+    ViewPager viewPager;
+    TabLayout tabLayout;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
+
+        setupViewPager();
     }
 
+    private void setupViewPager() {
+        viewPager.setAdapter(new PagerAdapter(getChildFragmentManager(), buildFragments()));
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setText("Top Songs");
+        tabLayout.getTabAt(1).setText("Top Artists");
+
+    }
+
+    private ArrayList<Fragment> buildFragments() {
+        ArrayList<Fragment> fragments = new ArrayList<>();
+
+        fragments.add(new TopSongsFragment());
+        fragments.add(new TopArtistFragment());
+
+        return fragments;
+    }
+
+    @Override
+    protected int getFragmentLayout() {
+        return R.layout.fragment_main;
+    }
 }
