7b09fe56272c0ac39f3d117e927335642c084aff
Alan Aliaga <alanaliaga@MacBook-Pro-de-Alan.local>
2016-04-18 20:20:49 -0400

    Refactorizacion de tabs a clase abstracta anadida

--- app/src/main/java/com/archer/amaterasu/ui/fragment/MainFragment.java
+++ app/src/main/java/com/archer/amaterasu/ui/fragment/MainFragment.java
@@ -6,46 +6,36 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.View;
-
 import com.archer.amaterasu.R;
-import com.archer.amaterasu.common.BaseFragment;
-import com.archer.amaterasu.utils.PagerAdapter;
-
-import java.util.ArrayList;
+import com.archer.amaterasu.utils.SetupTabsLayout;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends BaseFragment {
+public class MainFragment extends SetupTabsLayout {
 
+    @Bind(R.id.viewPager)
     ViewPager viewPager;
+    @Bind(R.id.tabLayout)
     TabLayout tabLayout;
+
+    private Fragment[] tabsFragments = {
+            new TopSongsFragment(),
+            new TopArtistFragment()
+    };
+
+    private String[] tabsTitles = {
+            "Top Songs",
+            "Top Artists"
+    };
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        viewPager = (ViewPager) view.findViewById(R.id.viewPager);
-        tabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
-
-        setupViewPager();
-    }
-
-    private void setupViewPager() {
-        viewPager.setAdapter(new PagerAdapter(getChildFragmentManager(), buildFragments()));
-        tabLayout.setupWithViewPager(viewPager);
-
-        tabLayout.getTabAt(0).setText("Top Songs");
-        tabLayout.getTabAt(1).setText("Top Artists");
-
-    }
-
-    private ArrayList<Fragment> buildFragments() {
-        ArrayList<Fragment> fragments = new ArrayList<>();
-
-        fragments.add(new TopSongsFragment());
-        fragments.add(new TopArtistFragment());
-
-        return fragments;
+        ButterKnife.bind(view);
+        setupViewPager(viewPager, tabLayout, tabsFragments, tabsTitles);
     }
 
     @Override
--- app/src/main/java/com/archer/amaterasu/utils/SetupTabsLayout.java
+++ app/src/main/java/com/archer/amaterasu/utils/SetupTabsLayout.java
@@ -1,4 +1,51 @@
 package com.archer.amaterasu.utils;
 
-public interface SetupTabsLayout {
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import com.archer.amaterasu.common.BaseFragment;
+import java.util.ArrayList;
+
+public abstract class SetupTabsLayout extends BaseFragment {
+    public void setupViewPager(ViewPager viewPager, TabLayout tabLayout, Fragment[] fragments, String[] tabsTitles) {
+        viewPager.setAdapter(new PagerAdapter(getChildFragmentManager(), buildFragments(fragments)));
+        tabLayout.setupWithViewPager(viewPager);
+
+        setTabsName(tabLayout, tabsTitles);
+    }
+
+    public ArrayList<Fragment> buildFragments(Fragment[] fragmentsArray) {
+
+        ArrayList<Fragment> fragments = new ArrayList<>();
+
+        for (Fragment fragment : fragmentsArray) {
+            fragments.add(fragment);
+        }
+
+        return fragments;
+    }
+
+    public void setTabsName(TabLayout tabLayout, String[] tabsTitles){
+        int size = tabsTitles.length;
+        for (int i = 0; i < size; i++) {
+            tabLayout.getTabAt(i).setText(tabsTitles[i]);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
